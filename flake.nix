32a97462d45d50acae7bff8012b53dee9671fa3c
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2025-08-01 15:10:49 +0300

    setup nix flakes

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Dev environment with Linux build dependencies";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }:
+
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          curl
+          git
+          gcc
+          flex
+          bison
+          ncurses.dev
+          openssl.dev
+          bc
+          elfutils.dev
+          pahole
+          pkg-config
+          perl
+        ];
+      };
+    };
+}
