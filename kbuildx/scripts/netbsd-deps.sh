6f68f8e37803fb7082afb50d2b2bdd851667664e
Tsiry Sandratraina <tsiry.sndr@rocksky.app>
2026-08-15 00:31:24 +0300

    install NetBSD native build toolchain

--- kbuildx/scripts/netbsd-deps.sh
+++ kbuildx/scripts/netbsd-deps.sh
@@ -79,7 +79,8 @@ mkdir -p /usr/pkg/lib/perl5/5.42.0/TAP/Formatter /usr/pkg/bin /usr/pkg/sbin /usr
 
 export ASSUME_ALWAYS_YES=yes
 pkgin update
-pkgin -y install git mozilla-rootcerts-openssl || cat /var/db/pkgin/pkg_install-err.log
+pkgin install git gcc binutils gmake bison flex perl mozilla-rootcerts-openssl \
+    || cat /var/db/pkgin/pkg_install-err.log
 if command -v mozilla-rootcerts >/dev/null 2>&1; then
     mozilla-rootcerts install
 elif [ -x /usr/pkg/sbin/mozilla-rootcerts ]; then
